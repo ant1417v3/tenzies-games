d4003a51242a5f3a534b636bd3f3032d8fc91b4d
Kennnnn <109525291+ant1417v3@users.noreply.github.com>
2023-06-16 02:50:52 +1000

    update funcion

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 
 import Die from './Die'
+import React from 'react'
+
 
 function App() {
 
+  const [dices, setDices] = React.useState(allNewDice())
+   
+  function allNewDice(){
+    const newArray = []
+    for (let i = 0; i < 10; i++) {
+      newArray.push(Math.floor(Math.random() *6))
+      
+    }
+    return newArray
+    
+  }
+  const diceElement = dices.map(dice  => <Die value={dice}></Die>)
+
+  function rollDice(){
+    setDices(allNewDice())
+  }
 
   return (
     <main>
       <div className='dice-container'>
-      < Die value="1" />
-      < Die value="1" />
-      < Die value="5" />
-      < Die value="1" />
-      < Die value="1" />
-      < Die value="1" />
-      < Die value="1" />
-      < Die value="1" />
-
+        {diceElement}
       </div>
+      <button className='roll-dice' onClick={rollDice}>Roll</button>
     </main>
 
   )
