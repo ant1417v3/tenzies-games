b677b3a09adab6672f0d587721ca7601b53e273c
Kennnnn <109525291+ant1417v3@users.noreply.github.com>
2023-06-15 11:50:24 +1000

    Init code

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import Die from './Die'
+
+function App() {
+
+
+  return (
+    <main>
+      <div className='dice-container'>
+      < Die value="1" />
+      < Die value="1" />
+      < Die value="5" />
+      < Die value="1" />
+      < Die value="1" />
+      < Die value="1" />
+      < Die value="1" />
+      < Die value="1" />
+
+      </div>
+    </main>
+
+  )
+}
+
+export default App
